ac37f910fc4481e80fe2a8d7df53f3d641bdc8cc
Artem Tarasov <artem@dintecom.com>
2021-05-13 21:46:07 -0400

    feat(package): Migrate to Angular 12 (#210)
    BREAKING CHANGE: Support only Angular 12

---
+++ projects/ngx-meta/core/src/lib/meta.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot } from '@angular/router';
+import { MetaService } from './meta.service';
+
+@Injectable()
+export class MetaGuard implements CanActivate, CanActivateChild {
+  constructor(private readonly meta: MetaService) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const url = state.url;
+
+    const metaSettings = route.hasOwnProperty('data') ? route.data.meta : undefined;
+
+    this.meta.update(url, metaSettings);
+
+    return true;
+  }
+
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    return this.canActivate(route, state);
+  }
+}
---
+++ projects/ngx-meta/core/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of core
+ */
+
+export * from './lib/meta.guard';
+export * from './lib/meta.loader';
+export * from './lib/meta.module';
+export * from './lib/meta.service';
+export * from './lib/models/meta-settings';
+export * from './lib/models/page-title-positioning';
